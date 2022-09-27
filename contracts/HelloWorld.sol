f89ab003fe98f12865078a5507b59f7151ab0496
qingxo <qingxo@sina.com>
2022-09-27 22:54:30 +0800

    init hello world constract

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Import this file to use console.log
+import "hardhat/console.sol";
+
+contract HelloWorld {
+    event UpdateMessages(string oldStr, string newStr);
+
+    string public message;
+
+    constructor(string memory initMessage) {
+        message = initMessage;
+    }
+
+    function update(string memory newMessage) public {
+        string memory oldMsg = message;
+        message = newMessage;
+        emit UpdateMessages(oldMsg, newMessage);
+    }
+}
