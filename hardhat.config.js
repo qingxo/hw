10450a7cd8402ea51e7737b737fe8da0ca18c45f
qingxo <qingxo@sina.com>
2022-09-28 17:34:50 +0800

    Error in plugin @nomiclabs/hardhat-etherscan: Failed to send contract verification request. Endpoint URL: https://api-goerli.etherscan.io/api Reason: Connect Timeout Error 这个错误，经过分析，是 https://api-goerli.etherscan.io/api网络问题，无法访问，后续看怎么去解决

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,6 @@ require("@nomiclabs/hardhat-etherscan");
 
 const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
